f0f8c1b42676e8e2113680a6b934e2b59a61fd90
angetheo <angelotomasco@gmail.com>
2015-06-11 21:11:51 +0200

    adds logic for navigation to recognize active tabs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,17 @@
 get '/dashboard' do
   if authenticate?
-    erb :'dashboard/index', :layout => :'dashboard/layout'
+
+    #NAVIGATION
+    @current_tab = !!params[:tab] ? params[:tab] : ""
+    case @current_tab
+    when ''
+      erb :'dashboard/index', :layout => :'dashboard/layout'
+    when 'seo'
+      erb :'dashboard/seo', :layout => :'dashboard/layout'
+    else
+      erb :'dashboard/index', :layout => :'dashboard/layout'
+    end
+
   else
     flash[:error] = "Accedi per visualizzare il contenuto."
     redirect '/login'
